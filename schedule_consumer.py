53a1362cceb81bf4de3eb5862e2b5f44d24afeed
i521652 <kfir.wilfand@sap.com>
2020-09-13 20:11:34 +0300

    not work

--- schedule_consumer.py
+++ schedule_consumer.py
@@ -11,8 +11,7 @@ import posix_ipc
 
 # Utils for this demo
 import utils
-
-SHARED_MEMORY_PREFIX = "SHARED_MEMORY"
+ = "SHARED_MEMORY"
 SCHEDULE_INTERVAL = 0.1
 PY_MAJOR_VERSION = sys.version_info[0]
 params = utils.read_params()
@@ -22,14 +21,7 @@ def consume_job(shared_memory_name_key):
     print("I'm working..." + shared_memory_name_key)
     # Create the shared memory and the semaphore.
     memory = posix_ipc.SharedMemory(params[shared_memory_name_key])
-
-    # MMap the shared memory
-    mapfile = mmap.mmap(memory.fd, memory.size)
-
-    # Once I've mmapped the file descriptor, I can close it without
-    # interfering with the mmap.
-    memory.close_fd()
-
+dsjfasdfp'io'
     s = utils.read_from_memory(mapfile)
     print(s)
 
